6414819a9de33fd11d2b23bb173846e073eb7d85
LhgS <lucas.henrique.gois.santos@gmail.com>
2025-06-14 09:47:45 -0300

    Adicionando a main e includes da main

---
+++ code/src/main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+#include "raylib.h"
+
+#define BOARD_SIZE 8
+#define TILE_SIZE 42
+
+int main(void) {
+
+    return 0;
+}
